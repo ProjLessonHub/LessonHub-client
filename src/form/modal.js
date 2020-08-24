7d3104f521e6241d950eccb3130f9dc3553b71cb
Chris Padilla <mrchrispadilla@gmail.com>
2020-08-24 18:09:19 -0500

    Fix eslint warnings

--- src/form/modal.js
+++ src/form/modal.js
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
     Dialog,
-    Paper,
-    PaperProps,
     DialogTitle,
     Typography,
-    Tabs,
     DialogContent,
-    Slide
+    Slide,
 } from '@material-ui/core'
-// import Draggable from 'react-draggable'
 
-import DeleteIcon from '@material-ui/icons/Delete'
-import EditIcon from '@material-ui/icons/Edit'
-import SaveIcon from '@material-ui/icons/Save'
-import CopyIcon from '@material-ui/icons/FileCopy'
-import SendIcon from '@material-ui/icons/Send'
-import CloseIcon from '@material-ui/icons/Close'
-import AddIcon from '@material-ui/icons/Add'
-
-// function PaperComponent(props) {
-//     return (
-//         <Draggable
-//             handle="#draggable-dialog-title"
-//             cancel={'[class*="MuiDialogContent-root"]'}
-//         >
-//             <Paper {...props} />
-//         </Draggable>
-//     )
-// }
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const Modal = ({ children, open, onClose, title, }) => {
+const Modal = ({ children, open, onClose, title }) => {
 
     return (
         <Dialog
@@ -50,7 +28,7 @@ const Modal = ({ children, open, onClose, title, }) => {
                     flexDirection: 'row',
                     justifyContent: 'space-between',
                     alignItems: 'center',
-                    width: '100%'
+                    width: '100%',
                 }}>
                     <Typography variant="h6" color="textPrimary">
                         {title}
--- src/home/home.js
+++ src/home/home.js
@@ -6,7 +6,6 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  Typography,
   InputBase,
   Drawer,
   Divider,
@@ -21,8 +20,6 @@ import {
   ChevronRight,
   LibraryAdd,
 } from '@material-ui/icons'
-import LessonCard from '../card/lessonCard'
-import NestedGrid from '../card/nestedGrid'
 import getLessons from '../api/getLessons'
 import searchLesson from '../api/searchLesson'
 import getLessonsByCat from '../api/getLessonsByCat'
@@ -124,6 +121,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(0, 1),
+
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
@@ -176,6 +174,7 @@ const useStyles = makeStyles((theme) => ({
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
+
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
@@ -211,15 +210,6 @@ const category = [
   { name: 'Foreign Languages', value: 'FL' },
   { name: 'Social Studies', value: 'SS' },
   { name: 'Fitness', value: 'Fitness' },
-  // { name: 'Grade 4', value: 4 },
-  // { name: 'Grade 5', value: 5 },
-  // { name: 'Grade 6', value: 6 },
-  // { name: 'Grade 7', value: 7 },
-  // { name: 'Grade 8', value: 8 },
-  // { name: 'Grade 9', value: 9 },
-  // { name: 'Grade 10', value: 10 },
-  // { name: 'Grade 11', value: 11 },
-  // { name: 'Grade 12', value: 12 },
 ]
 
 const Home = () => {
